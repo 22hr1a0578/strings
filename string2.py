c8b26e3ea51115ab031d6aa5d73de792cfd84749
N.latha <154883112+22hr1a0578@users.noreply.github.com>
2024-10-22 10:49:25 +0530

    Create string2.py

---
+++ string2.py
@@ -0,0 +1,13 @@
+Write a program to print th length of the given string.
+Sample Input:
+Hello
+Sample Output:
+Hello: 5
+# Get input from the user
+input_string = input("Enter a string: ")
+
+# Calculate the length of the string
+length_of_string = len(input_string)
+
+# Print the result
+print(f"{input_string}: {length_of_string}")
